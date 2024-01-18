e8cecdd229763e9cd628d235d33495b1dad7d907
egrozell <ethan.grozell@ucalgary.ca>
2024-01-18 14:54:29 -0800

    started question 2

---
+++ examples/question2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def main():
+# question 2 i
+    r = 2.48e-2
+    r_std = 0.02e-2
+    h = 10.1e-2
+    h_std = 0.03e-2
+    a = np.pi()*r**2
+    amax = np.pi()*(r+r_std)**2
+    amin = np.pi()*(r-r_std)**2
+    print (f"""
+
+    """
+            )
+
+if __name__ == "__main__":
+    main()
